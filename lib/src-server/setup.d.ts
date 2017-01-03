588d10e75099e3f44569a673c5cf12fdac940a3b
Rick Love <rick@toldpro.com>
2017-01-02 21:29:48 -0600

    Add Setup

---
+++ lib/src-server/setup.d.ts
@@ -0,0 +1,2 @@
+import * as T from '@told/azure-functions-server/lib/src';
+export declare function main(context: T.Context<any>, request: T.Request<any, any>): void;
---
+++ src-server/setup.ts
@@ -0,0 +1,17 @@
+import * as T from '@told/azure-functions-server/lib/src';
+
+export function main(context: T.Context<any>, request: T.Request<any, any>) {
+
+    let html = '<html><head></head><body>Hello!!!</body></html>';
+
+    context.done(null, {
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Content-Type': 'text/html',
+        },
+        body: {
+            ok: true,
+            data: html as any,
+        }
+    });
+}
