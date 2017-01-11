781c68ef44b09247b90f535dd82b9bbd512cf133
Rick Love <rick@toldpro.com>
2017-01-10 18:34:38 -0600

    Test Serve Shopify-Script

--- src-server/shopify-script.ts
+++ src-server/shopify-script.ts
@@ -1,13 +1,17 @@
 import * as T from '@told/azure-functions-server/lib/src';
 
 declare let require: any;
+declare let __dirname: string;
 const fs = require('fs');
+const p = require('path') as { resolve(...parts: string[]): string };
 
 export async function main(context: T.Context<any>, request: T.Request<any, any>) {
 
-    let path = './resources/shopify-script.js';
+    let path = p.resolve(__dirname, 'resources', 'shopify-script.js');
 
     fs.readFile(path, (err: any, data: any) => {
+        context.log('path=' + path + ' data=' + data + ' err=' + err);
+
         let body = '' + data;
 
         context.done(null, {
